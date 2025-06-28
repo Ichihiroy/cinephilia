8b579a595c440c11c032f41e914f4156da3fb79f
Yunis Quliyev <realyanisquliyev@gmail.com>
2025-06-28 22:09:13 +0400

    feat: Implement Footer component with navigation links and styling

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,44 @@
 import React from "react";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <footer className="px-4 py-8 bg-gray-700 text-gray-100">
+      <div className="container flex flex-wrap items-center justify-center mx-auto space-y-4 sm:justify-between sm:space-y-0">
+        <div className="flex flex-row pr-3 space-x-4 sm:space-x-8">
+          <div className="flex items-center justify-center flex-shrink-0 w-12 h-12 rounded-full dark:bg-red-600"></div>
+          <ul className="flex flex-wrap items-center space-x-4 sm:space-x-8">
+            <li>
+              <a rel="noopener noreferrer" href="#">
+                Terms of Use
+              </a>
+            </li>
+            <li>
+              <a rel="noopener noreferrer" href="#">
+                Privacy
+              </a>
+            </li>
+          </ul>
+        </div>
+        <ul className="flex flex-wrap pl-3 space-x-4 sm:space-x-8">
+          <li>
+            <a rel="noopener noreferrer" href="#">
+              Instagram
+            </a>
+          </li>
+          <li>
+            <a rel="noopener noreferrer" href="#">
+              Facebook
+            </a>
+          </li>
+          <li>
+            <a rel="noopener noreferrer" href="#">
+              Twitter
+            </a>
+          </li>
+        </ul>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
