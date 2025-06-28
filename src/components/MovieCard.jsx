194a9eec13924650077031de97c468fe74fb0433
Yunis Quliyev <realyanisquliyev@gmail.com>
2025-06-28 22:58:40 +0400

    refactor: Enhance MovieCard component layout and improve flag emoji rendering

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,13 +1,44 @@
+function getFlagEmoji(code) {
+  return code
+    .toUpperCase()
+    .split("")
+    .map((char) => String.fromCodePoint(127397 + char.charCodeAt()))
+    .join("");
+}
+
 const MovieCard = ({ name, image, ageLimit, languages }) => (
   <div
-    className="bg-gray-700 rounded-lg overflow-hidden  shadow-lg w-full h-72 bg-center bg-cover hover:scale-105 transition-transform duration-300"
+    className="relative bg-gray-700 rounded-lg overflow-hidden shadow-lg w-full h-84 bg-center bg-cover hover:scale-105 transition-transform duration-300"
     style={{ backgroundImage: `url('${image}')` }}
   >
-    <div className="p-3 flex flex-col justify-end h-full bg-opacity-50">
+    <div className="absolute inset-0 bg-black opacity-50"></div>
+
+    <div className="relative p-3 flex flex-col justify-end h-full">
       <h3 className="text-white text-md font-semibold">{name}</h3>
-      <p className="text-sm text-gray-300">19.04.2025</p>
-      <p className="text-sm">
-        {ageLimit} <span>{languages[0]}</span>
+      <p className="text-sm text-gray-300 text-xs mt-2">19.04.2025</p>
+      <p className="text-sm flex justify-between items-center ">
+        {ageLimit === "SIXTEEN" ? (
+          <span className="text-xs">16+</span>
+        ) : ageLimit === "TWELVE" ? (
+          <span className="text-xs">12+</span>
+        ) : ageLimit === "EIGHTEEN" ? (
+          <span className="text-xs">18+</span>
+        ) : ageLimit === "SIX" ? (
+          <span className="text-xs">6+</span>
+        ) : null}{" "}
+        <span>
+          {languages.map((lang, i) =>
+            lang == "EN" ? (
+              <span key={i} className="mr-1">
+                {getFlagEmoji("GB")}
+              </span>
+            ) : (
+              <span key={i} className="mr-1">
+                {getFlagEmoji(lang)}
+              </span>
+            )
+          )}
+        </span>
       </p>
     </div>
   </div>
