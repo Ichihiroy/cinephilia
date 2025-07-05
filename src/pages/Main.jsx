0814fbd22170810573ac4bb493c888cc15fcc6da
Yunis Quliyev <realyanisquliyev@gmail.com>
2025-07-05 22:11:08 +0400

    feat: Implement Main component with movie and theatre data fetching and filtering

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-import MovieCard from "./MovieCard";
+import MovieCard from "../components/MovieCard";
 
 const Main = () => {
   const [data, setData] = useState([]);
