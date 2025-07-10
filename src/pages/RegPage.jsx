71a4aa9f2d4c08f27f0a6152792c99712839b10b
Yunis Quliyev <realyanisquliyev@gmail.com>
2025-07-10 19:37:04 +0400

    feat: Enhance registration page with user validation and toast notifications

--- src/pages/RegPage.jsx
+++ src/pages/RegPage.jsx
@@ -1,9 +1,16 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import toast from "react-hot-toast";
 import { LuEye, LuEyeClosed } from "react-icons/lu";
+import { getAllUsers, postUser } from "../services/authServices";
+import { useNavigate } from "react-router";
 
 const RegPage = () => {
   const [showPassword, setShowPassword] = useState(false);
   const [showConfirmPassword, setShowConfirmPassword] = useState(false);
+  const [secPass, setSecPass] = useState("");
+  const [users, setUsers] = useState([]);
+
+  const navigate = useNavigate();
 
   const [userData, setUserData] = useState({
     name: "",
@@ -13,11 +20,21 @@ const RegPage = () => {
     birthdate: "",
   });
 
+  useEffect(() => {
+    getAllUsers().then((data) => {
+      setUsers(data);
+    });
+  }, []);
+
   function handleChange(e) {
     setUserData({
       ...userData,
       [e.target.name]: e.target.value,
     });
+  }
+
+  function handlePassChange(e) {
+    setSecPass(e.target.value);
   }
 
   function registerUser() {
@@ -28,8 +45,34 @@ const RegPage = () => {
       !userData.email ||
       !userData.birthdate
     ) {
-      alert("Bütün sahələri doldurun!");
+      toast.error("Bütün sahələri doldurun!");
       return;
+    } else if (
+      users.some(
+        (user) => user.email === userData.email || user.phone === userData.phone
+      )
+    ) {
+      toast.error("Bu e-poçt və ya telefon nömrəsi artıq mövcuddur!");
+      return;
+    } else if (userData.password !== secPass) {
+      toast.error("Şifrələr uyğun gəlmir!");
+      return;
+    } else {
+      postUser(userData);
+
+      toast.success("Qeydiyyat uğurla tamamlandı!");
+
+      setTimeout(() => {
+        navigate("/");
+      }, 1000);
+
+      setUserData({
+        name: "",
+        surname: "",
+        phone: "",
+        email: "",
+        birthdate: "",
+      });
     }
   }
 
@@ -39,7 +82,7 @@ const RegPage = () => {
     <div className="min-h-screen bg-zinc-900 text-white flex items-center justify-center px-4">
       <div className="w-full max-w-4xl">
         <h1 className="text-3xl font-semibold mb-8">Qeydiyyat</h1>
-        <form className="grid grid-cols-1 md:grid-cols-2 gap-6">
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
           <div>
             <label className="block mb-1">Ad</label>
             <input
@@ -118,6 +161,8 @@ const RegPage = () => {
             <label className="block mb-1">Şifrəni Təsdiqlə</label>
             <div className="flex items-center border-b border-gray-500">
               <input
+                name="confirmPassword"
+                onChange={(e) => handlePassChange(e)}
                 type={showConfirmPassword ? "text" : "password"}
                 className="w-full p-2 bg-transparent focus:outline-none"
               />
@@ -134,13 +179,12 @@ const RegPage = () => {
           <div className="md:col-span-2">
             <button
               onClick={registerUser}
-              type="submit"
-              className="w-full bg-red-800 hover:bg-red-700 transition text-white py-2 rounded-full"
+              className="w-full cursor-pointer bg-red-800 hover:bg-red-700 transition text-white py-2 rounded-full"
             >
               Qeydiyyat
             </button>
           </div>
-        </form>
+        </div>
       </div>
     </div>
   );
--- src/services/authServices.js
+++ src/services/authServices.js
@@ -1,2 +1,21 @@
 import axios from "axios";
 const API_URL = import.meta.env.VITE_API_URL;
+
+export const postUser = async (userData) => {
+  try {
+    const response = await axios.post(`${API_URL}/users`, userData);
+    return response.data;
+  } catch (error) {
+    console.error("Error registering user:", error);
+    throw error;
+  }
+};
+
+export const getAllUsers = async () => {
+  try {
+    const res = await axios.get(`${API_URL}/users`);
+    return res.data;
+  } catch (err) {
+    console.error("Error fetching users:", err);
+  }
+};
